5a7fe2f7210b257e0e54cebe2817df31eccf2f42
smdatt <steven.datt@gmail.com>
2018-04-18 16:53:32 +0100

    up to 2.3.6.6

--- manifests/security_options.pp
+++ manifests/security_options.pp
@@ -13,10 +13,9 @@ class cis_windows_level1_memberserver::security_options {
 
   # Set Ensure Accounts: Block Microsoft accounts is set to Users can't add or log on with Microsoft accounts 
   # cis-block-microsoft-accounts-2.3.1.2
-  registry::value { 'NoConnectedUser':
-    key  => 'HKLM\Software\Microsoft\Windows\CurrentVersion\Policies\System',
-    type => 'dword',
-    data => 3,
+  local_security_policy { 'Accounts: Block Microsoft accounts':
+    ensure       => present,
+    policy_value => '3',
   }
 
   # Set Ensure Accounts: Guest account status is set to Disabled
@@ -28,10 +27,9 @@ class cis_windows_level1_memberserver::security_options {
 
   # Set  Ensure Accounts: Limit local account use of blank passwords to console logon only is set to Enabled
   # cis-accounts-local-account-blank-passwords-2.3.1.4
-  registry::value { 'LimitBlankPasswordUse':
-    key  => 'HKLM\System\CurrentControlSet\Control\Lsa',
-    type => 'dword',
-    data => 1,
+  local_security_policy { 'Accounts: Limit local account use of blank passwords to console logon only':
+    ensure       => present,
+    policy_value => '1',
   }
 
   # Set Configure Accounts: Rename administrator account
@@ -51,17 +49,75 @@ class cis_windows_level1_memberserver::security_options {
   # Set Ensure Audit: Force audit policy subcategory settings (Windows Vista or later) to override audit policy category settings' is set to 'Enabled'
   # cis-audit-subcateogory-override-2.3.2.1
   local_security_policy { 'Audit: Force audit policy subcategory settings (Windows Vista or later) to override audit policy category settings':
+    ensure => present,
+    policy_value => '1'
+  }
+
+  # Set Ensure Audit: Shut down system immediately if unable to log security audits is set to Disabled'
+  # cis-shutdown-when-unable-to-log-audits-2.3.2.2
+  local_security_policy { 'Audit: Shut down system immediately if unable to log security audits':
+    ensure => present,
+    policy_value => '0'
+  }
+
+  # Set Ensure Devices: Allowed to format and eject removable media is set to Administrators
+  # cis-format-media-2.3.4.1
+  local_security_policy { 'Devices: Allowed to format and eject removable media':
+    ensure => present,
+    policy_value => '0'
+  }
+
+  # Set Ensure Devices: Prevent users from installing printer drivers is set to Enabled
+  # cis-prevent-printer-drivers-2.3.4.2
+  local_security_policy { 'Devices: Prevent users from installing printer drivers':
+    ensure => present,
+    policy_value => '1'
+  }
+
+  # Set Ensure Domain member: Digitally encrypt or sign secure channel data (always) is set to Enabled
+  # cis-encrypt-sign-channel-data-always-2.3.6.1
+  local_security_policy { 'Domain member: Digitally encrypt or sign secure channel data (always)':
+    ensure => present,
+    policy_value => '1'
+  }
+
+  # Set Ensure Domain member: Digitally encrypt secure channel data (when possible) is set to Enabled
+  # cis-encrypt-channel-data-when-possible-2.3.6.2
+  local_security_policy { 'Domain member: Digitally encrypt secure channel data (when possible)':
+    ensure => present,
+    policy_value => '1'
+  }
+
+  # Set Ensure Domain member: Digitally sign secure channel data (when possible) is set to Enabled
+  # cis-sign-channel-data-when-possible-2.3.6.3
+  local_security_policy { 'Domain member: Digitally sign secure channel data (when possible)':
+    ensure => present,
+    policy_value => '1'
+  }
+
+
+
+  # Set Ensure Domain member: Disable machine account password changes is set to Disabled
+  # cis-disable-machine-password-changes-2.3.6.4
+  local_security_policy { 'Domain member: Disable machine account password changes':
+    ensure => present,
+    policy_value => '0'
+  }
+
+  # Set Ensure Domain member: Maximum machine account password age is set to 30 or fewer days, but not 0
+  # cis-machine-account-password-age-2.3.6.5
+  local_security_policy { 'Domain member: Maximum machine account password age':
+    ensure       => present,
+    policy_value => '5',
+  }
+
+  # Set Ensure Domain member: Require strong (Windows 2000 or later) session key is set to Enabled
+  # cis-strong-session-key-2.3.6.6
+  local_security_policy { 'Domain member: Require strong (Windows 2000 or later) session key':
     ensure       => present,
     policy_value => '1',
   }
-  
-  # Set Ensure Audit: Shut down system immediately if unable to log security audits is set to Disabled'
-  # cis-shutdown-when-unable-to-log-audits-2.3.2.2
-  registry::value { 'CrashOnAuditFail':
-    key  => 'HKLM\System\CurrentControlSet\Control\Lsa',
-    type => 'dword',
-    data => 0,
-  }
 
-
+  # Set Ensure Interactive logon: Do not display last user name is set to Enabled
+  # cis-logon-dont-display-last-user-2.3.7.1
 }
