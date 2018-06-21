c995401318702aa53ee860aaf6a4931ed4fe41ed
smdatt <steven.datt@gmail.com>
2018-06-21 11:08:59 +0100

    wip edit

--- manifests/user_templates.pp
+++ manifests/user_templates.pp
@@ -8,7 +8,7 @@ class cis_windows_level1_memberserver::user_templates {
   file {
     'C:\\Windows\\system32\\grouppolicy':
     ensure => 'directory',
-    source => 'puppet:///modules/localmodules/cis-windows2012r2-level1-memberserver-puppet/files/',
+    source => 'puppet:///modules/cis-windows2012r2-level1-memberserver-puppet/files/',
     path   => 'C:\\Windows\\system32\\grouppolicy'
   }
 }
