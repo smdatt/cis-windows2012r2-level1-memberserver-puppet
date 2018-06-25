95bc7e83217643d9a83679806ec621d48f7560de
smdatt <steven.datt@gmail.com>
2018-06-25 15:52:52 +0100

    removing user templates - user template file not working correctly

--- manifests/user_templates.pp
+++ manifests/user_templates.pp
@@ -4,11 +4,12 @@
 #
 
 class cis_windows_level1_memberserver::user_templates {
-
+/*
   file {
     'C:\\Windows\\system32\\grouppolicy':
     ensure => 'directory',
     source => 'puppet:///modules/cis-windows2012r2-level1-memberserver-puppet/files/',
     path   => 'C:\\Windows\\system32\\grouppolicy'
   }
+*/
 }
